70efb666b5f0da9eda6b6a004bbcbfb412a0e8b1
KHAN MAHMUD ARIF RABBANI <snigdho70@gmail.com>
2025-05-08 22:19:55 +0600

    show password and hide it is implimented

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,6 +1,11 @@
-import { GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import {
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+} from "firebase/auth";
 import React, { use, useEffect, useState } from "react";
-import { FaGoogle } from "react-icons/fa";
+import { FaEye, FaEyeSlash, FaGoogle } from "react-icons/fa";
 import auth from "../Firebase/Firebase.init";
 import { Authconext } from "../Provider/AuthProvider";
 import { Link, useLocation, useNavigate } from "react-router";
@@ -8,66 +13,96 @@ import { Link, useLocation, useNavigate } from "react-router";
 const Login = () => {
   const navigate = useNavigate();
   const location = useLocation();
-  const {user, setUser} = use(Authconext);
-    const handleSignIn = (email, password) => {
-      signInWithEmailAndPassword(auth, email, password)
+  const { user, setUser } = use(Authconext);
+  const handleSignIn = (email, password) => {
+    signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
-          const user = userCredential.user;
-          // console.log(user);
-          navigate(`${location.state? location.state : '/'}`);
-      }).catch((error) => {
-          const errorCode = error.code;
-          const errorMessage = error.message;
-          // console.error(errorCode, errorMessage);
-          alert(errorMessage);
-      }
-      );
-    }
-    
+        const user = userCredential.user;
+        // console.log(user);
+        navigate(`${location.state ? location.state : "/"}`);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // console.error(errorCode, errorMessage);
+        alert(errorMessage);
+      });
+  };
 
-    const googleProvider = new GoogleAuthProvider();
-    const handleGoogleSignIn = () => {
-        signInWithPopup(auth, googleProvider)
-        .then(result => {
-            const user = result.user;
-            setUser(user);
-            navigate(`${location.state? location.state : '/'}`);
-            
-        })
-        .catch(error => {
-            // console.error("Error signing in with Google: ", error);
-        });
-    }
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const form = e.target;
-        const email = form.email.value;
-        const password = form.password.value;
+  const [showPassword, setShowPassword] = useState(false);
 
-        handleSignIn(email, password);
-        // console.log(email, password);
-    }
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+  const googleProvider = new GoogleAuthProvider();
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        const user = result.user;
+        setUser(user);
+        navigate(`${location.state ? location.state : "/"}`);
+      })
+      .catch((error) => {
+        // console.error("Error signing in with Google: ", error);
+      });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+
+    handleSignIn(email, password);
+    // console.log(email, password);
+  };
 
   return (
     <div className="hero ">
-        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-          <div className="card-body">
-            <h1 className="text-center text-2xl font-bold">Login</h1>
-            <form onSubmit={handleSubmit} className="fieldset">
-              <label className="label">Email</label>
-              <input type="email" name="email" className="input" placeholder="Email" />
-              <label className="label">Password</label>
-              <input type="password" name="password" className="input" placeholder="Password" />
-              <div>
-                <a className="link link-hover">Forgot password?</a>
-              </div>
-              <button className="btn btn-neutral mt-4" type="submit">Login</button>
-            </form>
-            <button onClick={handleGoogleSignIn} className="btn mt-3 bg-amber-100"> <FaGoogle />Login with Google</button>
-            <h1 className="text-center mt-5">Don't Have an Account? Please <Link to='/resister' className="underline text-blue-500">Resister</Link></h1>
-          </div>
+      <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+        <div className="card-body">
+          <h1 className="text-center text-2xl font-bold">Login</h1>
+          <form onSubmit={handleSubmit} className="fieldset">
+            <label className="label">Email</label>
+            <input
+              type="email"
+              name="email"
+              className="input"
+              placeholder="Email"
+            />
+            <label className="label">Password</label>
+            <div className="flex items-center">
+              <input
+                type={showPassword ? "text" : "password"}
+                name="password"
+                className="input"
+                placeholder="Password"
+              />
+              <div className="absolute right-14 text-lg" onClick={togglePasswordVisibility}>{showPassword ? <FaEyeSlash /> : <FaEye />  }</div>
+            </div>
+            <div>
+              <a className="link link-hover">Forgot password?</a>
+            </div>
+            <button className="btn btn-neutral mt-4" type="submit">
+              Login
+            </button>
+          </form>
+          <button
+            onClick={handleGoogleSignIn}
+            className="btn mt-3 bg-amber-100"
+          >
+            {" "}
+            <FaGoogle />
+            Login with Google
+          </button>
+          <h1 className="text-center mt-5">
+            Don't Have an Account? Please{" "}
+            <Link to="/resister" className="underline text-blue-500">
+              Resister
+            </Link>
+          </h1>
         </div>
       </div>
+    </div>
   );
 };
 

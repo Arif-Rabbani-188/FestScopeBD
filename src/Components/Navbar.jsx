8de26c5090dd086596e28f49bc3ddc236a7879cd
KHAN MAHMUD ARIF RABBANI <snigdho70@gmail.com>
2025-05-08 22:37:52 +0600

    text size changed of logo

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -84,7 +84,7 @@ const Navbar = () => {
             </li>
           </ul>
         </div>
-        <a className="btn btn-ghost text-lg md:text-4xl">FestScope BD</a>
+        <a className="btn btn-ghost text-2xl md:text-4xl">FestScope BD</a>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
